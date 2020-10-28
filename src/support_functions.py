1bf8ba52de5126b4299c1bbd73a0c6000b919374
Thomas Joy <thomasjoy@Thomass-MacBook-Air.local>
2020-10-28 17:54:16 -0400

    End of Wednesday

--- src/support_functions.py
+++ src/support_functions.py
@@ -10,8 +10,6 @@ from uszipcode import SearchEngine
 plt.style.use('ggplot')
 
 
-
-
 def get_zip_income():
     '''Gets all median incomes per zip code in NYC via uszipcode'''
     
@@ -19,28 +17,40 @@ def get_zip_income():
     zipcode = search.by_zipcode("10001")
     zipcode
 
-    data_by_zip = pd.read_csv('../data/data-by-modzcta.csv')
+    data_by_zip = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/data-by-modzcta.csv')
     zip_lst = data_by_zip['MODIFIED_ZCTA']
 
-    # zipcode.values() # to list
+
     zipcode_lst = list(zip_lst)
     median_income = []
+    keys = ['median_household_income', 'lat', 'lng', 'population', 'population_density']
     for i in zipcode_lst:
         zc = search.by_zipcode(i)
-        median_income.append((i, zc.to_dict()['median_household_income'])) # to dict
-    
-    
-    return pd.DataFrame(median_income, columns=['zip', 'median_income'])
+        zd = zc.to_dict()
+        zd_lst = [zd.get(key) for key in keys]
+        median_income.append(([i]+zd_lst)) # to dict
+        
+
+    return pd.DataFrame(median_income, columns=['zip', 'median_household_income', 'lat', 'lng', 'population', 'population_density'])
 
 
 def get_covid_data():
     '''Retreaves All COVID-19 Data via CSV's '''
     
-    data_by_zip = pd.read_csv('../data/data-by-modzcta.csv')
-    tests_by_day = pd.read_csv('../data/tests.csv')
-    tests_by_boro = pd.read_csv('../data/by-boro.csv')
+    data_by_zip = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/data-by-modzcta.csv')
+    tests_by_day = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/tests.csv')
+    tests_by_boro = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/by-boro.csv')
     
     return data_by_zip, tests_by_day, tests_by_boro
+
+
+def get_turnstile_data():
+    '''Retreaves All Turnstile Data for 2019 & 2020'''
+    
+    turnstile2019_df = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/Turnstile_Usage_Data__2019.csv', low_memory=False)
+    turnstile2020_df = pd.read_csv('~/Desktop/galvanize/NYC-MTA-Usage-During-COVID-19/data/Turnstile_Usage_Data__2020.csv', low_memory=False)
+    
+    return turnstile2019_df, turnstile2020_df
 
 
 def fixing_datetime(df):
